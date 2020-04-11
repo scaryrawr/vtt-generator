f56299a8a02aee7219e195db4e9e8038d571f8d4
Mike Wallio <miwalli@microsoft.com>
2020-04-11 12:23:38 -0400

    Add a max line duration

--- vtt-gen.py
+++ vtt-gen.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
 import argparse
+import contextlib
+import json
+import math
+import os
+import time
+import wave
+
 import azure.cognitiveservices.speech as azurespeech
 import ffmpeg
-import time
 import yaml
-import json
-import os
-import wave
-import contextlib
 
 parser = argparse.ArgumentParser(description='Generate VTT for video file')
 parser.add_argument('--input', type=str, help='input video file')
 parser.add_argument('--output', type=str, help='output file')
+parser.add_argument('-maxlinetime', type=float, help='max line time in seconds', default=2.5)
 
 args = parser.parse_args()
 
@@ -53,34 +56,56 @@ def stop_cb(evt):
     done = True
 
 sequence = 0
+
+def write_chunk(chunk):
+    global sequence
+    global outfile    
+    sequence += 1
+    timeline = '{} --> {}'.format(format_timestamp(chunk['start']), format_timestamp(chunk['end']))
+    outfile.write('{}\n{}\n{}\n\n'.format(sequence, timeline, chunk['text'].strip()))
+
+max_chunk_length = args.maxlinetime * 10000000.0
 def recognized_cb(evt):
+    global max_chunk_length
+
     # Need to load results from json to be able to get offset and duration
     result = json.loads(evt.result.json)
 
     # Offset is start time
-    start = format_timestamp(result['Offset'])
+    chunk = {}
+    chunk['start'] = result['Offset']
+    chunk['text'] = ''
+    
+    confidences_in_nbest = [item['Confidence'] for item in result['NBest']]
+    best_index = confidences_in_nbest.index(max(confidences_in_nbest))
+
+    # Assuming words and dispaly words length are the same...
+    words = result['NBest'][best_index]['Words']
+    display_words = result['DisplayText'].split(' ')
+    
+    last_end = 0
+    for i in range(len(words)):
+        word = words[i]
+        chunk['end'] = word['Offset'] + word['Duration']
+        chunk['text'] = f"{chunk['text']} {display_words[i]}"
+        if (chunk['end'] - chunk['start'] > max_chunk_length):
+            write_chunk(chunk)
+            chunk['start'] = chunk['end']
+            chunk['text'] = ''
+            last_end = chunk['end']
+
+    # the last bit of text might not be a "full" line
+    if (last_end != chunk['end']):
+        write_chunk(chunk)
 
     # Add the duration to get the end time
     end_pos = result['Offset'] + result['Duration']
-    end = format_timestamp(end_pos)
-    
-    global sequence
-    global outfile
-    
-    # Format is:
-    # Sequence#
-    # Start --> End
-    # Captions
-    # [blank line]
-    sequence += 1
-    timeline = '{} --> {}'.format(start, end)
-    outfile.write('{}\n{}\n{}\n\n'.format(sequence, timeline, result['DisplayText']))
-
     global duration
     print('\rProgress: {:.2f}%'.format(end_pos / 100000.0 / duration), end='')
-    
 
 speech_config = azurespeech.SpeechConfig(subscription=config['key'], region=config['region'])
+speech_config.request_word_level_timestamps()
+
 audio_config = azurespeech.audio.AudioConfig(filename=audio_file)
 
 speech_recognizer = azurespeech.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
